55dbcb030050d633f6a17f985336896d4bf588b7
Mihai <M.Lefter@lumc.nl>
2021-03-04 20:53:53 +0100

    Add version

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,8 +3,19 @@
     <v-layout>
       <v-flex xs12>
         <h1 class="display-1 mt-10">About Page</h1>
-        <p>Under development.</p>
+        <br />
+        <p>Website version: {{ version }}</p>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {
+  computed: {
+    version() {
+      return process.env.VUE_APP_VERSION;
+    },
+  },
+};
+</script>
