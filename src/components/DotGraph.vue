870454ab8ee5ca2d11a48c453d29b59a41d3a0e6
mlefter <m.lefter@lumc.nl>
2022-09-14 12:14:26 +0200

    Svg export

--- src/components/DotGraph.vue
+++ src/components/DotGraph.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <v-btn icon color="primary" @click="getSvg">
+      <v-icon>mdi-download</v-icon>
+    </v-btn>
     <div
       id="dot-graph"
       style="
@@ -34,6 +37,44 @@ export default {
         .height(this.width / 3)
         .dot(this.dottext)
         .render();
+    },
+    getSvg() {
+      var svg = document.getElementById("dot-graph");
+
+      var serializer = new XMLSerializer();
+      var svg_content = serializer.serializeToString(svg);
+      svg_content = svg_content.substring(svg_content.indexOf(">") + 1);
+      svg_content = svg_content.replace("</div>", "");
+
+      //add name spaces.
+      if (
+        !svg_content.match(/^<svg[^>]+xmlns="http:\/\/www\.w3\.org\/2000\/svg"/)
+      ) {
+        svg_content = svg_content.replace(
+          /^<svg/,
+          '<svg xmlns="http://www.w3.org/2000/svg"'
+        );
+      }
+      if (
+        !svg_content.match(/^<svg[^>]+"http:\/\/www\.w3\.org\/1999\/xlink"/)
+      ) {
+        svg_content = svg_content.replace(
+          /^<svg/,
+          '<svg xmlns:xlink="http://www.w3.org/1999/xlink"'
+        );
+      }
+
+      //add xml declaration
+      svg_content = '<?xml version="1.0" standalone="no"?>\r\n' + svg_content;
+
+      var outputContent =
+        "data:image/svg+xml;charset=utf-8," + encodeURIComponent(svg_content);
+
+      var download_link = document.createElement("a");
+      download_link.setAttribute("href", outputContent);
+      download_link.setAttribute("download", "graph.svg");
+      document.body.appendChild(download_link);
+      download_link.click();
     },
   },
 };
