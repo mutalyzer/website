1d38d0fbb174522c3a7946454017e3a12920f77b
Mihai Lefter <mihai.lefter@gmail.com>
2024-04-05 13:34:18 +0200

    WIP: protein view alternative

--- src/components/AffectedProtein.vue
+++ src/components/AffectedProtein.vue
@@ -24,6 +24,10 @@
       <div v-if="this.fancy_protein" class="protein-seq">
         <pre v-html="this.fancy_protein"></pre>
       </div>
+      <div v-if="this.fancy_protein_2" class="overline">Fancy</div>
+      <div v-if="this.fancy_protein_2" class="protein-seq">
+        <pre v-html="this.fancy_protein_2"></pre>
+      </div>
     </div>
   </div>
 </template>
@@ -44,11 +48,13 @@ export default {
       reference: [],
       predicted: [],
       fancy_protein: null,
+      fancy_protein_2: null,
     };
   },
   created: function () {
     [this.reference, this.predicted] = this.getParts(this.protein);
     this.fancy_protein = this.fancy(this.protein);
+    this.fancy_protein_2 = this.fancy_2(this.protein);
   },
   methods: {
     reverseString(s) {
@@ -127,6 +133,83 @@ export default {
       }
       return rows.join("<br>");
     },
+    fancy_2(protein) {
+      let chunk_length = 10;
+      let columns = 6;
+      console.log(columns);
+      let chunks = this.chunkSplit(protein.reference, chunk_length);
+
+      if (
+        "position_first" in protein &&
+        "position_last_original" in protein &&
+        "position_last_predicted" in protein
+      ) {
+        console.log("we should get the chunk index and seq index");
+        console.log(protein.position_first);
+        console.log(protein.position_last_original);
+        console.log(protein.position_last_predicted);
+
+        let after_i = Math.floor(protein.position_last_original / chunk_length);
+        let after_i_i =
+          protein.position_last_original -
+          Math.floor(protein.position_last_original / chunk_length) *
+            chunk_length;
+        if (after_i == chunks.length) {
+          after_i--;
+          after_i_i = chunks[chunks.length - 1].length;
+        }
+        console.log("should insert at ", after_i, after_i_i);
+        chunks[after_i] = this.insertInto(chunks[after_i], after_i_i, "</b>");
+
+        let before_i = Math.floor(protein.position_first / chunk_length);
+        let before_i_i =
+          protein.position_first -
+          Math.floor(protein.position_first / chunk_length) * chunk_length;
+        chunks[before_i] = this.insertInto(
+          chunks[before_i],
+          before_i_i,
+          '<b style="color:#FF0000">'
+        );
+      }
+      console.log(chunks);
+
+      let last_pos_index =
+        Math.floor(protein.reference.length / (chunk_length * columns)) *
+          (chunk_length * columns) +
+        1;
+      let last_pos_index_length = last_pos_index.toString().length;
+      let rows = [];
+      for (
+        let row = 0;
+        row < Math.ceil(protein.reference.length / (chunk_length * columns));
+        row++
+      ) {
+        console.log("row", row);
+        let current_row_fancy = [];
+        current_row_fancy.push(
+          (chunk_length * columns * row + 1)
+            .toString()
+            .padStart(last_pos_index_length, " ")
+        );
+        for (let col = 0; col < columns; col++) {
+          console.log(row * columns + col);
+          current_row_fancy.push(chunks[row * columns + col]);
+        }
+        rows.push(current_row_fancy.join(" "));
+      }
+      console.log(rows);
+      return rows.join("<br>");
+    },
+    insertInto(s, i, c) {
+      return s.slice(0, i) + c + s.slice(i);
+    },
+    chunkSplit(s, chunk_length) {
+      let chunks = [];
+      for (let i = 0; i < s.length; i += chunk_length) {
+        chunks.push(s.substr(i, chunk_length));
+      }
+      return chunks;
+    },
   },
 };
 </script>
@@ -151,7 +234,8 @@ export default {
   margin-left: 5px;
   padding: 10px;
   text-decoration: none;
-  font-family: monospace;
+  font-size: 13px;
+  font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
   display: block;
   color: #004d40;
   background-color: #efefef;
